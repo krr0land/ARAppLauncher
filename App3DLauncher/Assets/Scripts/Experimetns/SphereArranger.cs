a21804e4136c2ddca694847e3ac025cef38e3578
csiztom <tom.csiz@gmail.com>
2023-04-23 11:37:21 +0200

    arrange update

--- App3DLauncher/Assets/Scripts/Experimetns/SphereArranger.cs
+++ App3DLauncher/Assets/Scripts/Experimetns/SphereArranger.cs
@@ -17,10 +17,10 @@ public class SphereArranger : MonoBehaviour
 
         for (int i = 0; i < objectCount; i++)
         {
-            float phi = Mathf.Acos(1f - (2f * i + 1f) / objectCount);
+            float phi = Mathf.Acos(1f - (2f * (i * 0.8f + objectCount * 0.1f) + 1f) / objectCount);
             float theta = Mathf.PI * (1f + Mathf.Sqrt(5f)) * (i + 1f);
 
-            Vector3 newPos = new Vector3(Mathf.Cos(theta) * Mathf.Sin(phi), Mathf.Sin(theta) * Mathf.Sin(phi), Mathf.Cos(phi)) * radius;
+            Vector3 newPos = new Vector3(Mathf.Cos(theta) * Mathf.Sin(phi), Mathf.Cos(phi), Mathf.Sin(theta) * Mathf.Sin(phi)) * radius;
             Quaternion rotation = Quaternion.LookRotation(newPos - transform.position);
 
             arrangedItems[i].transform.position = transform.position + newPos;
